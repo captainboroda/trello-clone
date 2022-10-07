20523953551a55afb7c07df5b12fb23a4c379646
captain.boroda <captain.boroda@gmail.com>
2022-10-07 10:43:34 +0300

    add config module and service, change typeorm module config to be async

--- apps/api/src/app/app.module.ts
+++ apps/api/src/app/app.module.ts
@@ -8,24 +8,29 @@ import {UserEntity} from "./users/entities/user.entity";
 import {ProfileEntity} from "./profiles/entities/profile.entity";
 import { ProfilesModule } from './profiles/profiles.module';
 import { AuthModule } from './auth/auth.module';
+import { ConfigModule, ConfigService } from "@nestjs/config";
 
 @Module({
   imports: [
-    UsersModule,
-    TypeOrmModule.forRoot({
-      type: 'postgres',
-      host: 'localhost',
-      port: 5432,
-      username: 'postgres',
-      password: 'lol',
-      database: 'trello',
-      logNotifications: true,
-      entities: [
-        UserEntity,
-        ProfileEntity
-      ],
-      synchronize: true,
+    ConfigModule.forRoot({
+      isGlobal: true,
     }),
+    TypeOrmModule.forRootAsync({
+      imports: [ConfigModule],
+      useFactory: (configService: ConfigService) => ({
+        type: 'postgres',
+        host: configService.get('DB_HOST'),
+        port: +configService.get<number>('DB_PORT'),
+        username: configService.get('DB_USERNAME'),
+        password: configService.get('DB_PASSWORD'),
+        database: configService.get('DB_NAME'),
+        logNotifications: true,
+        entities: [UserEntity, ProfileEntity],
+        synchronize: true,
+      }),
+      inject: [ConfigService],
+    }),
+    UsersModule,
     ProfilesModule,
     AuthModule
   ],
